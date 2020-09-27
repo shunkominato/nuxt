731fa7e54abd702ed9e7bc218ac2e5a8f7300734
15951595 <shun19871001@gmail.com>
2020-09-27 22:46:38 +0900

    router set

---
+++ pages/users/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="container">
+    <p>{{ message }}</p>
+    <hr />
+    <router-link to="/price">price page</router-link>
+  </section>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "hellow",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
